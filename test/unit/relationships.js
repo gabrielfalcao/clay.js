9ad9b3341e2344b77a7c5108422cdae6bb1539ed
Gabriel Falcão <gabriel.falcao@nytimes.com>
2023-06-05 04:29:49 +0000

    wip

--- test/unit/relationships.js
+++ test/unit/relationships.js
@@ -1,36 +1,36 @@
-/* -*- coding: utf-8 -*-
-<clay - active record for node.js with redis backend>
-Copyright (C) <2011>  Gabriel Falcão <gabriel@yipit.com>
+// -*- coding: utf-8 -*-
+// <clay - active record for node.js with redis backend>
+// Copyright (C) <2011>  Gabriel Falcão <gabriel@yipit.com>
+// Copyright (C) <2023>  Gabriel Falcão <gabrielteratos@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
 
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following
-conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE. */
-
-var vows = require('vows'),
-    should = require('should'),
+const should = require('should'),
     _ = require('underscore')._,
     crypto = require('crypto');
 
 _old_include = should.include;
 
-var models = require('../../clay');
+var models = require('../../lib/clay');
 
 should.include = function (where, what, msg){
     var found = false;
@@ -53,9 +53,9 @@ should.include = function (where, what, msg){
     }
 };
 
-var mock = new models.storage.Mechanism();
+var inMemory = new models.storage.Mechanism();
 
-models.set_primary_storage(mock);
+models.set_primary_storage(inMemory);
 
 var User = models.declare("User", function(it, kind){
     it.has.field("name", kind.string);
@@ -82,22 +82,22 @@ var BuildInstruction = models.declare("BuildInstruction", function(it, kind){
     it.has.one("owner", User, "created_instructions");
 });
 
-vows.describe('Relationships').addBatch({
-    'metadata has a *"relationships"* attribute': function(){
+describe('Relationships', () => {
+    test('metadata has a *"relationships"* attribute', () => {
         should.include(BuildInstruction._meta, 'relationships');
-    },
-    'metadata.relationships is an object': function(){
+    })
+    test('metadata.relationships is an object', () => {
         should.isObject(BuildInstruction._meta.relationships);
-    },
-    '_meta.relationships has *one to many* thru attribute *"has_many"*': function(){
+    })
+    test('_meta.relationships has *one to many* thru attribute *"has_many"*', () => {
         should.include(BuildInstruction._meta.relationships, 'has_many');
         should.isArray(BuildInstruction._meta.relationships.has_many);
-    },
-    '_meta.relationships has *many to one* thru attribute *"has_one"*': function(){
+    })
+    test('_meta.relationships has *many to one* thru attribute *"has_one"*', () => {
         should.include(BuildInstruction._meta.relationships, 'has_one');
         should.isArray(BuildInstruction._meta.relationships.has_one);
-    },
-    '*one to many* is defined in the declared model': function(){
+    })
+    test('*one to many* is defined in the declared model', () => {
         should.include(
             BuildInstruction._meta.relationships.has_one,
             {
@@ -106,8 +106,8 @@ vows.describe('Relationships').addBatch({
                 reverse_name: 'created_instructions'
             }
         );
-    },
-    '*one to many* is defined in the referred *(side efffected)*model': function(){
+    })
+    test('*one to many* is defined in the referred *(side efffected)*model', () => {
         should.include(
             User._meta.relationships.has_many,
             {
@@ -116,8 +116,8 @@ vows.describe('Relationships').addBatch({
                 reverse_name: 'owner'
             }
         );
-    },
-    '*many to one* is defined in the declared model': function(){
+    })
+    test('*many to one* is defined in the declared model', () => {
         should.include(
             BuildInstruction._meta.relationships.has_one,
             {
@@ -126,8 +126,8 @@ vows.describe('Relationships').addBatch({
                 reverse_name: 'created_instructions'
             }
         );
-    },
-    '*many to one* is defined in the referred *(side efffected)*model': function(){
+    })
+    test('*many to one* is defined in the referred *(side efffected)*model', () => {
         should.include(
             User._meta.relationships.has_many,
             {
@@ -136,10 +136,11 @@ vows.describe('Relationships').addBatch({
                 reverse_name: 'owner'
             }
         );
-    },
-    '*has one* creates a clever property that sets from an object to a model': {
-        topic: function(){
-            return new BuildInstruction({
+    })
+  describe('*has one* creates a clever property that sets from an object to a model', () => {
+    test(
+      'the immediate relationship is converted to a model instance', function(){
+            const b = new BuildInstruction({
                 name: "Yipit Data",
                 repository_address: 'git@github.com:Yipit/data.git',
                 owner: {
@@ -148,14 +149,13 @@ vows.describe('Relationships').addBatch({
                     password: 'got you a gift'
                 }
             });
-        },
-        'the immediate relationship is converted to a model instance': function(b){
-            should.isObject(b.owner._meta);
-            should.equal(b.owner.name, 'Adam Nelson');
+
+        should.isObject(b.owner._meta);
+        should.equal(b.owner.name, 'Adam Nelson');
             should.equal(b.owner.greet(), "Hello, my name is Adam Nelson, it's nice to meet you");
         }
-    },
-    '*has many* creates a clever property that sets from an array of objects': function() {
+    )
+    test('*has many* creates a clever property that sets from an array of objects', function() {
         var b = new BuildInstruction({
             name: "Yipit Data",
             repository_address: 'git@github.com:Yipit/data.git',
@@ -194,8 +194,8 @@ vows.describe('Relationships').addBatch({
         should.equal(b.builds[0].status, 0);
         should.equal(b.builds[0].error, '');
         should.equal(b.builds[0].output, 'cool');
-    },
-    '*one-to-many* self-assignment to the affected instance': function(){
+    })
+    test('*one-to-many* self-assignment to the affected instance', () => {
         var Owner = models.declare('Owner', function(it, kind){
             it.has.field('name', kind.string);
         });
@@ -218,8 +218,8 @@ vows.describe('Relationships').addBatch({
 
         o1.items[0].owner.name.should.equal('John Doe');
         o1.items[1].owner.name.should.equal('John Doe');
-    },
-    '*one-to-many* fething by reverse name': function(){
+    })
+    test('*one-to-many* fething by reverse name', () => {
         var Owner = models.declare('Owner', function(it, kind){
             it.has.field('name', kind.string);
         });
@@ -247,8 +247,8 @@ vows.describe('Relationships').addBatch({
 
         _.map(i1.owner.items, function(i){ return i.__data__.__id__; }).should.include(i2.__id__);
         _.map(i1.owner.items, function(i){ return i.__data__.__id__; }).should.include(i1.__id__);
-    },
-    '*many-to-one* self-assignment to the affected instance': function(){
+    })
+    test('*many-to-one* self-assignment to the affected instance', () => {
         var Item = models.declare('Item', function(it, kind){
             it.has.field('description', kind.string);
         });
@@ -271,8 +271,8 @@ vows.describe('Relationships').addBatch({
 
         o1.items[0].owner.name.should.equal('John Doe');
         o1.items[1].owner.name.should.equal('John Doe');
-    },
-    '*many-to-one* fething by reverse name': function(){
+    })
+    test('*many-to-one* fething by reverse name', () => {
         var Item = models.declare('Item', function(it, kind){
             it.has.field('description', kind.string);
         });
